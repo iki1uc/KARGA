72130995e004c7a5e71231d1d80911a609679624
iki1uc <entertrainer@web.de>
2026-08-14 02:37:36 +0200

    Add karga function to process and save data

--- KARGA.js
+++ KARGA.js
@@ -1,1 +1,15 @@
+import { core } from "./CORE.js";
+import { diagnose } from "./DIAG.js";
+import { save } from "./STORE.js";
+import { ourMode } from "./OUR.js";
+
+export function karga(name) {
+  const result = core(name);
+  const diag   = diagnose(result);
+  const team   = ourMode(result);
+
+  save(team);
+
+  return { result, diag, team };
+}
 
