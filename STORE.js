131abbe8d2af0666931b7f741b51b6d12b42a43b
iki1uc <entertrainer@web.de>
2026-08-14 02:36:53 +0200

    Update STORE.js

--- STORE.js
+++ STORE.js
@@ -1,1 +1,8 @@
+export function save(result) {
+  localStorage.setItem(result.name, JSON.stringify(result));
+}
+
+export function load(name) {
+  return JSON.parse(localStorage.getItem(name));
+}
 
