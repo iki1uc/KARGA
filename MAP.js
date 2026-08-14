35004b066ef098e69b21ab078b1c462e79d3d2f5
iki1uc <entertrainer@web.de>
2026-08-14 02:34:58 +0200

    Implement mapModule function in MAP.js
    Added mapModule function that processes name input.

--- MAP.js
+++ MAP.js
@@ -1,1 +1,13 @@
+import { detectType } from "./ID.js";
+import { detectAxis } from "./AXIS.js";
+import { inRange } from "./RANGE.js";
+
+export function mapModule(name) {
+  const type = detectType(name);
+  const num  = parseInt(name.match(/\d+/));
+  const axis = detectAxis(num);
+  const ok   = inRange(type, num);
+
+  return { name, type, num, axis, ok };
+}
 
