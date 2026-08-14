9a5aac3fcfe27fcae32de3b0dc4f70ba8c1f791d
iki1uc <entertrainer@web.de>
2026-08-14 02:34:11 +0200

    Add detectAxis function to classify numbers

--- AXIS.js
+++ AXIS.js
@@ -1,1 +1,7 @@
+export function detectAxis(num) {
+  if (num >= 1 && num <= 81) return "O";
+  if (num >= 82 && num <= 162) return "Z";
+  if (num >= 163 && num <= 243) return "T";
+  return "X"; // Fehlerachse
+}
 
