df3c6ee46cd2daff25b1fc0420239e60729766ba
iki1uc <entertrainer@web.de>
2026-08-14 02:34:38 +0200

    Add inRange function to check number ranges

--- RANGE.js
+++ RANGE.js
@@ -1,1 +1,7 @@
+export function inRange(type, num) {
+  if (type === "tmp" && num <= 81) return true;
+  if (type === "vec" && num >= 82 && num <= 162) return true;
+  if (type === "re"  && num >= 163 && num <= 243) return true;
+  return false;
+}
 
